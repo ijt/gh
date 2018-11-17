c19c8056b28b145276f1ce158fc4c4c7e6570b3d
Issac Trotts <issactrotts@google.com>
2018-11-16 19:27:56 -0800

    restructure to have subcommands

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ package main
 import (
 	"context"
 	"fmt"
+	"io"
 	"log"
 	"os"
 	"text/tabwriter"
@@ -12,21 +13,45 @@ import (
 )
 
 func main() {
+	if len(os.Args) != 1+1 {
+		fmt.Fprintf(os.Stderr, `usage: gh COMMAND
+
+GitHub from the command line
+
+commands:
+
+  pulls		list open pull requests
+`)
+		os.Exit(1)
+	}
+	cmd := os.Args[1]
+
+	w := tabwriter.NewWriter(os.Stdout, 0, 8, 2, ' ', 0)
 	client := github.NewClient(nil)
 	owner := mustGetenv("GITHUB_OWNER")
 	repo := mustGetenv("GITHUB_REPO")
+
+	switch cmd {
+	case "pulls":
+		pulls(client, w, owner, repo)
+	default:
+		fmt.Fprintf(os.Stderr, "unknown command: %s\n", cmd)
+		os.Exit(1)
+	}
+	w.Flush()
+}
+
+func pulls(client *github.Client, w io.Writer, owner, repo string) {
 	prs, _, err := client.PullRequests.List(context.Background(), owner, repo, nil)
 	if err != nil {
 		log.Fatal(err)
 	}
-	w := tabwriter.NewWriter(os.Stdout, 0, 8, 2, ' ', 0)
 	var pr *github.PullRequest
 	for _, pr = range prs {
 		if *pr.State == "open" {
 			fmt.Fprintf(w, "%s\t%s\t%s\n", *pr.User.Login, *pr.HTMLURL, *pr.Title)
 		}
 	}
-	w.Flush()
 }
 
 func mustGetenv(name string) string {
